d4a8d9a19beea8201d45804994b154ec00442897
RachelTWQ <racheltang15@hotmail.com>
2019-04-08 21:51:07 +0000

    printing each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,39 @@
 var request = require('request');
+var secret = require('./GITHUB_TOKEN');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoCountributors(repoOwner, repoName, cb){
-
+function getRepoCountributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': 'token ' + secret.GITHUB_TOKEN
+        }
+      };
+    
+    request(options, function (err, res, body) {
+        if(err){
+            throw err;
+        }
+        console.log(res.statusCode);
+        if (res.statusCode === 200){
+            var data = JSON.parse(body);
+            var avatarUrl;
+            for (let i = 0; i < data.length; i++){
+                avatarUrl = data[i]["avatar_url"];
+                console.log("avatar_url is ", avatarUrl);
+            }
+        }
+    }
+    
+    
+    );
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoCountributors("nodejs", "node", function (err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
 });
+
+//User-Agent: curl/7.47.0
