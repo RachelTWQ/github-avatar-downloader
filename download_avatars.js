278386c3eaf005516a3fb35ace5b21ddf88b5ed9
RachelTWQ <racheltang15@hotmail.com>
2019-04-09 02:53:57 +0000

    add command line arguments and filter empty entry

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,15 @@ var request = require('request');
 var fs = require('fs');
 var secret = require('./GITHUB_TOKEN');
 
+var owner = process.argv[2];
+var name = process.argv[3];
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoCountributors(repoOwner, repoName, cb) {
+if (owner == null || name == null){
+    console.log('Please provide valid repoOwner and repoName.')
+} else {
+    function getRepoCountributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
@@ -21,38 +27,39 @@ function getRepoCountributors(repoOwner, repoName, cb) {
         if (res.statusCode === 200){
             var data = JSON.parse(body);
             cb(null, data);
-            
         }
-        
     });
 }
 
 function downloadImageByURL(url, filePath){
+
     request.get(url)
     .on('error', function(err){
         throw err;
     })
     .on('response', function (response){
         if (response.statusCode === 200){
-            console.log('Download started');
-        }
+            console.log('Download started'); 
+            // type = response.headers['content-type'].split('/')[1];
+            // console.log('Type is: ', type);
+        }  
     })
     .pipe(fs.createWriteStream(filePath))
     .on('finish', function(){
-        console.log("Download completed");
-    })
+         console.log("Download completed")
+     });
     
 }
 
-getRepoCountributors("nodejs", "node", function (err, data) {
+getRepoCountributors(owner, name, function (err, result) {
     // var avatarUrl = {};
-    for (let i = 0; i < data.length; i++){
+    for (let i = 0; i < result.length; i++){
         //avatarUrl[data[i]["login"]] = data[i]["avatar_url"];
-        var url =  data[i]['avatar_url'];
-        var filePath = './avatars/' + data[i]['login'];
-        downloadImageByURL(url, filePath)
+        var url =  result[i]['avatar_url'];
+        var filePath = './avatars/' + result[i]['login'];
+        downloadImageByURL(url, filePath);
     }
     // return downloadImageByURL(avatarUrl);
 });
+}
 
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg");
